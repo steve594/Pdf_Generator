f579f895410aae5a1074429815b6ca831bd9574a
Steve Njock <stevenjockchristian@gmail.com>
2023-12-14 21:58:26 -0500

    Initial commit for my Pdf Generator

---
+++ main.py
@@ -0,0 +1,17 @@
+#Import FPDF
+from fpdf import FPDF
+import pandas as pd
+
+#Define the file shape
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+df = pd.read_csv("topics.csv")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+
+    pdf.set_font(family="Times", style="B", size=12)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
+
+
+
+pdf.output("pdf_gen.pdf")
